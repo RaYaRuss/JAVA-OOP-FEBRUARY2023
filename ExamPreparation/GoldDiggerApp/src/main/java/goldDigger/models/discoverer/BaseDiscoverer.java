2307fd728ac52cfe7b086937806b8ba4389cea25
Radina Yaneva <radina_yaneva@yahoo.com>
2023-03-27 16:17:35 +0300

    Add

---
+++ ExamPreparation/GoldDiggerApp/src/main/java/goldDigger/models/discoverer/BaseDiscoverer.java
@@ -0,0 +1,66 @@
+package goldDigger.models.discoverer;
+
+import goldDigger.models.museum.BaseMuseum;
+import goldDigger.models.museum.Museum;
+
+import static goldDigger.common.ExceptionMessages.DISCOVERER_ENERGY_LESS_THAN_ZERO;
+import static goldDigger.common.ExceptionMessages.DISCOVERER_NAME_NULL_OR_EMPTY;
+
+public abstract class BaseDiscoverer implements Discoverer{
+    private static final double ENERGY_COST_OF_DIGGING = 15;
+
+    private String name;
+    private double energy;
+    private Museum museum;
+
+    protected BaseDiscoverer(String name, double energy) {
+        this.setName(name);
+        this.setEnergy(energy);
+        museum = new BaseMuseum();
+    }
+    public void dig() {
+//        if (energy < ENERGY_COST_OF_DIGGING) {
+//            energy = 0;
+//        } else {
+//            energy = energy - ENERGY_COST_OF_DIGGING;
+//        }
+        energy = Math.max(0, energy - ENERGY_COST_OF_DIGGING);
+    }
+
+    public boolean canDig() {
+        return energy > 0;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(DISCOVERER_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    @Override
+    public double getEnergy() {
+        return energy;
+    }
+
+    protected void setEnergy(double energy) {
+        if (energy < 0) {
+            throw new IllegalArgumentException(DISCOVERER_ENERGY_LESS_THAN_ZERO);
+        }
+        this.energy = energy;
+    }
+
+    @Override
+    public Museum getMuseum() {
+        return museum;
+    }
+
+    protected void setMuseum(Museum museum) {
+        this.museum = museum;
+    }
+}
---
+++ ExamPreparation/GoldDiggerApp/src/main/java/goldDigger/models/operation/OperationImpl.java
@@ -0,0 +1,26 @@
+package goldDigger.models.operation;
+
+import goldDigger.models.discoverer.Discoverer;
+import goldDigger.models.spot.Spot;
+
+import java.util.Collection;
+
+public class OperationImpl implements Operation{
+
+    @Override
+    public void startOperation(Spot spot, Collection<Discoverer> discoverers) {
+        // take discoverer
+        //take exhibit
+        //remove exhibit form spot, add it in museum of discoverer
+
+        Collection<String> exhibitsInSpot = spot.getExhibits();
+        for (Discoverer discoverer : discoverers) {
+            while (discoverer.canDig() && exhibitsInSpot.iterator().hasNext()) {
+                discoverer.dig();
+                String currentExhibit = exhibitsInSpot.iterator().next();
+                discoverer.getMuseum().getExhibits().add(currentExhibit);
+                exhibitsInSpot.remove(currentExhibit);
+            }
+        }
+    }
+}
